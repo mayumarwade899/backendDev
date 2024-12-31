708a76f9d1be3ec72452b58ff20075bb22accc45
Mayur Marwade <mayumarwade899@gmail.com>
2025-01-01 00:09:35 +0530

    project8

---
+++ project8/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/userposts");
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  age: Number,
+  password: String,
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
+});
+
+module.exports = mongoose.model("user", userSchema);
