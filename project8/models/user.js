324072e4d16535f57b828e42add4ce2f2f83168c
Mayur Marwade <mayumarwade899@gmail.com>
2025-01-02 13:59:07 +0530

    project 8 final draft added

--- project8/models/user.js
+++ project8/models/user.js
@@ -8,6 +8,10 @@ const userSchema = mongoose.Schema({
   email: String,
   age: Number,
   password: String,
+  profilepic : {
+    type: String,
+    default: "default.png"
+  },
   posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
 });
 
